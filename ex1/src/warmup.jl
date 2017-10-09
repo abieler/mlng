3787ae5b197d7a89f1a5b4d296a146481d3359c4
abieler <andre.bieler81@gmail.com>
2017-10-09 22:08:36 +0200

    Initial commit

---
+++ ex1/src/warmup.jl
@@ -0,0 +1,10 @@
+function warm_up_exercise()
+# ============= YOUR CODE HERE ==============
+# Instructions: Return the 5x5 identity matrix
+#               In julia, functions return the last expression that is executed
+#               in the function. You do not need theexplicit return statement
+#               (but you can use it if you want).
+
+  eye(5)
+
+end
